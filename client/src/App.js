aee3a9701f55fc32a06f70425166a3c377f8437f
[SW7] 정현지 <blackit14@gmail.com>
2024-01-29 15:11:39 +0900

    fix: conflict 해결

--- client/src/App.js
+++ client/src/App.js
@@ -6,12 +6,12 @@ import AdminLayout from './layout/AdminLayout';
 
 // Home
 import Home from './pages/Home/Home';
+import Address from './pages/MyPage/Adress';
+import Order from './pages/Order/Order';
 
 // Products
 import ProductPage from './pages/Products/ProductPage';
 import ProductDetail from './pages/Products/ProductDetail';
-import Cart from './pages/Cart/Cart';
-import Order from './pages/Order/Order';
 
 // User
 import Login from './pages/Login/Login';
@@ -26,9 +26,9 @@ import ManageOrder from './pages/Admin/Order/ManageOrder';
 import ManageUser from './pages/Admin/User/ManageUser';
 
 // Mypage
-import MyPage from './pages/Mypage/MyPage';
-import UserOrder from './pages/Mypage/UserOrder/UserOrder';
-import Userinfo from './pages/Mypage/Userinfo/Userinfo';
+import MyPage from './pages/MyPage/MyPage';
+import UserOrder from './pages/MyPage/UserOrder/UserOrder';
+import Userinfo from './pages/MyPage/UserInfo/Userinfo';
 
 const Flex = styled.div`
   display: flex;
@@ -40,19 +40,18 @@ function App() {
       <Router>
         <Routes>
           <Route element={<MainLayout />}>
-            <Route path="/" element={<Home />}></Route>
-            <Route path="/products" element={<ProductPage />}></Route>
-            <Route path="/products/:listType" element={<ProductPage />}></Route>
-            <Route path="/products/detail/:id" element={<ProductDetail />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/order" element={<Order />} />
-            <Route path="/auth/login" element={<Login />}></Route>
-            <Route path="/auth/join" element={<Join />}></Route>
-            <Route path="/userinfo" element={<Userinfo />}></Route>
-            <Route path="/user/order" element={<UserOrder />}></Route>
-            <Route path="/user/:id" element={<Userinfo />}></Route>
-            <Route path="/user" element={<MyPage />}></Route>
-            <Route path="/user/settings" element={<Userinfo />}></Route>
+            <Route path='/' element={<Home />}></Route>
+            <Route path='/products' element={<ProductPage />}></Route>
+            <Route path='/products/:listType' element={<ProductPage />}></Route>
+            <Route path='/products/detail/:id' element={<ProductDetail />} />
+            <Route path='/order' element={<Order />} />
+            <Route path='/auth/login' element={<Login />}></Route>
+            <Route path='/auth/join' element={<Join />}></Route>
+            <Route path='/userinfo' element={<Userinfo />}></Route>
+            <Route path='/user/order' element={<UserOrder />}></Route>
+            <Route path='/user/:id' element={<Userinfo />}></Route>
+            <Route path='/user' element={<MyPage />}></Route>
+            <Route path='/user/settings' element={<Userinfo />}></Route>
           </Route>
         </Routes>
       </Router>
@@ -61,16 +60,16 @@ function App() {
         <Router>
           <Routes>
             <Route element={<AdminLayout />}>
-              <Route path="/category" element={<Category />}></Route>
+              <Route path='/category' element={<Category />}></Route>
               <Route
-                path="/manageproducts"
+                path='/manageproducts'
                 element={<ManageProducts />}
               ></Route>
-              <Route path="/addproduct" element={<AddProduct />}></Route>
-              <Route path="/manageorder" element={<ManageOrder />}></Route>
-              <Route path="/manageuser" element={<ManageUser />}></Route>
+              <Route path='/addproduct' element={<AddProduct />}></Route>
+              <Route path='/manageorder' element={<ManageOrder />}></Route>
+              <Route path='/manageuser' element={<ManageUser />}></Route>
               <Route
-                path="/productedit/:product_id"
+                path='/productedit/:product_id'
                 element={<ManageProductEdit />}
               ></Route>
             </Route>
